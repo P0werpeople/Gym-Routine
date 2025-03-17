111eee7d58eddd36f9b3696c9ac4b08592432abd
Sean Stara <sean.stara1@gmail.com>
2025-03-16 19:37:14 -0500

    Added Push and Pull Routine. Also made it not repeat and added a printing statement

--- Gym_Routine_2.0.py
+++ Gym_Routine_2.0.py
@@ -1,20 +1,57 @@
 import random
 
 class GymRoutine:
+    #Printing
+    def printing(self, hits):
+        i = 0
+        print("The hits for today are:")
+        while i < len(hits):
+            print(hits[i])
+            i += 1
+        
+    #Random Choosing for Routines
+    def add_hits(self, list):
+        hits = []
+        i = 0
+        while i < 6:
+            add_hit = random.choice(list)
+            hits.insert(i, add_hit)
+            list.remove(add_hit)
+            i += 1
+        return hits
+
+    #Leg Routine set up
     def legs(self):
         legs_list = ['Back Squat', 'Conventional Deadlift', 'Sumo Deadkift', 'Leg Press', 'Romanian deadlift(DB)', "Split Squat",
         "Leg Extension", "Lying Hamstring Curls", 'Cafe Raise']
-        legs_hit = []
-        i = 0
-        while i < 6:
-            legs_hit.insert(i, random.choice(legs_list))
-            i += 1
-        return legs_hit
+        hits = self.add_hits(legs_list)
+        return hits
+
+    #Push Routine Set up
+    def push(self):
+        push_list = ['Bench Press', 'Overhead Press', 'Incline DB Press', 'DB Lateral Raise', 'DB Chest Flyers', 
+        'Barbell Tricep Extensions', 'Rope Tricep Extensions', 'Cable Crossover']
+        hits = self.add_hits(push_list)
+        return hits
+
+    #Pull Routine Set Up
+    def pull(self):
+        pull_list = ['Deadlift', 'BB Row', 'Lat Pulldown', 'DB Row', 'Face Pull', 'BB Curl', 'Preacher Curl', 'Skull Crushers', 
+        'Shurgs', 'Cable Curls', 'Seated Rows']
+        hits = self.add_hits(pull_list)
+        return hits
+
     def main(self):
-        choice = int(input("What do you want to hit today: 1 - Legs, 2 - Push, 3 - Pull?"))
+        choice = int(input("What do you want to hit today: 1 - Legs, 2 - Push, 3 - Pull? "))
         if choice == 1:
             legday = self.legs()
-            print(legday)
+            self.printing(legday)
+        elif choice == 2:
+            pushday = self.push()
+            self.pringint(pushday)
+        elif choice == 3:
+            pullday = self.pull()
+            self.printing(pullday)
         
 if __name__ == '__main__':
     GymRoutine().main()
